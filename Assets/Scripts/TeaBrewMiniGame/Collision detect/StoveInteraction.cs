5e54786cac7a69b10b620aca595929c51a5bc194
maureen <79654365+maureen-n-n@users.noreply.github.com>
2025-10-23 21:31:15 -0700

    Updated minigame placement
    WASD minigame is only triggered when the mortar hits the stove
    Neg/Pos sound effects for timing bar (pouring) minigame implemented
    Timing bar game speed reduced and target point expanded - much easier to pass now

--- Assets/Scripts/TeaBrewMiniGame/Collision detect/StoveInteraction.cs
+++ Assets/Scripts/TeaBrewMiniGame/Collision detect/StoveInteraction.cs
@@ -41,7 +41,6 @@ public class StoveInteraction : MonoBehaviour
         if (other.CompareTag("Teapot") && game.teapotHasWater && game.teapotHasLeaves)
         {
             Debug.Log("Teapot placed on stove — boiling water...");
-            stirringMinigame.SetActive(true);
             game.teapotBoiled = true;
             Debug.Log("Tea boiled!");
         }
